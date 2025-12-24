99fa49256a349c1823beb782b5d5b4ff8d3c85e7
pcuser399 <turki.mahdi.tn@gmail.com>
2025-12-24 08:11:05 +0100

    sdads

---
+++ Load.js
@@ -0,0 +1,12 @@
+async function Load(CMD,info="10",obj={}){
+        const response = await fetch('https://nonextensively-monodomous-juana.ngrok-free.dev/submit',
+          {
+            method : 'POST',
+            headers:{'Content-Type':'application/json'},
+            body : JSON.stringify({ command: CMD , object : obj})
+          }
+        );
+        let data = await response.json();
+        return data.result ;
+
+}
